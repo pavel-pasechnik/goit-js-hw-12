8351493d97d451be98ba7e34eaa3d4794bd4e64a
Pavel <Pavel.Pasechnik@gmail.com>
2024-01-15 21:23:52 +0200

    added pagination

--- src/main.js
+++ src/main.js
@@ -8,109 +8,164 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 // Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
+// Підключення завантажувача
+import axios from 'axios';
 
-const input = document.querySelector('.search-input');
+const api = axios.create({
+  baseURL: 'https://pixabay.com/api/',
+  params: {
+    key: '41487030-c0d4f2e8fae3a5e9414bad560',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  },
+});
+
 const form = document.querySelector('.search-form');
 const ul = document.querySelector('.images-list');
-const divGallery = document.querySelector('.gallery');
-
-// Створення нового елементу для завантажувача
-const loaderElement = document.createElement('span');
-loaderElement.className = 'loader is-hidden';
-
-// Додавання завантажувача до списку
-divGallery.append(loaderElement);
-
-const loaderClass = document.querySelector('.loader');
-
-const BASE_URL = new URL('https://pixabay.com/api/');
-const KEY = '41487030-c0d4f2e8fae3a5e9414bad560';
-
+const loadMoreBtn = document.querySelector('.load_more_btn');
+const input = document.querySelector('.search-input');
 // Ініціалізація SimpleLightbox один раз поза обробником подій
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: '250',
 });
 
-form.addEventListener('submit', listener => {
-  listener.preventDefault();
-  // Переключення видимості завантажувача
-  loaderClass.classList.remove('is-hidden');
+// Функція виведення зображення на сторінку
+function renderImages(images = []) {
+  const insertImages = images.reduce(
+    (html, img) =>
+      html +
+      `
+      <li class="images-item">
+        <a class="images-link" href="${img.largeImageURL}"><img class="images" data-source="${img.largeImageURL}" alt="${img.tags}" src="${img.webformatURL}" width="360" height="200"></a>
+        <div class="description">
+        <div>
+          <p><b>Likes</b></p>
+          <p>${img.likes}</p>
+        </div>
+        <div>
+          <p><b>Views</b></p>
+          <p>${img.views}</p>
+        </div>
+        <div>
+          <p><b>Comments</b></p>
+          <p>${img.comments}</p>
+        </div>
+        <div>
+          <p><b>Downloads</b></p>
+          <p>${img.downloads}</p>
+        </div>
+        </div>
+      </li>
+        `,
+    ''
+  );
+  ul.insertAdjacentHTML('beforeend', insertImages);
+  // Виклик методу для оновлення галереї
+  lightbox.refresh();
+}
 
-  const query = listener.currentTarget.elements.query.value;
+// Виклик повідомлення з помилкою
+function errorAlert() {
+  iziToast.show({
+    title: '',
+    message:
+      'Sorry, there are no images matching your search query. Please try again!',
+    messageColor: '#FFFFFF',
+    backgroundColor: '#EF4040',
+    color: '#B51B1B',
+    iconUrl: './bi_x-octagon.svg',
+    iconColor: '#FAFAFB',
+    position: 'topRight',
+  });
+}
 
-  BASE_URL.searchParams.append('key', KEY);
-  BASE_URL.searchParams.append('q', query);
-  BASE_URL.searchParams.append('image_type', 'photo');
-  BASE_URL.searchParams.append('orientation', 'horizontal');
-  BASE_URL.searchParams.append('safesearch', 'true');
+// ============= Пагінація ============ //
 
-  fetch(BASE_URL)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
+// Функція створення запиту
+const getImages = async params => {
+  try {
+    const response = await api.get('', { params });
+    return response.data;
+  } catch (error) {
+    errorAlert();
+  }
+};
+
+// Функція для створення параметрів запиту
+const createGetImagesRequest = q => {
+  let page = 1;
+  let isLastPage = false;
+  const per_page = 3; // ! TO DO 40
+
+  return async () => {
+    try {
+      // Виходимо, якщо остання сторінка
+      if (isLastPage) return [];
+      // Робимо запит на першу сторінку
+      const { hits, totalHits } = await getImages({ page, per_page, q });
+
+      // Перевіряємо на наявність зображень згідно запиту
+      if (hits.length === 0) {
+        throw new Error();
       }
-      // Очищення параметрів запиту
-      BASE_URL.searchParams.delete('key');
-      BASE_URL.searchParams.delete('q');
-      BASE_URL.searchParams.delete('image_type');
-      BASE_URL.searchParams.delete('orientation');
-      BASE_URL.searchParams.delete('safesearch');
-      //
-      return response.json();
-    })
-    .then(images => {
-      input.value = '';
-      // Приховання завантажувача після отримання результатів
-      loaderClass.classList.add('is-hidden');
-      if (images.hits.length === 0) {
-        throw new Error(error);
-      }
-      ul.innerHTML = images.hits.reduce(
-        (html, img) =>
-          html +
-          `
-          <li class="images-item">
-            <a class="images-link" href="${img.largeImageURL}"><img class="images" data-source="${img.largeImageURL}" alt="${img.tags}" src="${img.webformatURL}" width="360" height="200"></a>
-            <div class="description">
-            <div>
-            <p><b>Likes</b></p>
-            <p>${img.likes}</p>
-            </div>
-            <div>
-            <p><b>Views</b></p>
-            <p>${img.views}</p>
-            </div>
-            <div>
-            <p><b>Comments</b></p>
-            <p>${img.comments}</p>
-            </div>
-           <div>
-           <p><b>Downloads</b></p>
-           <p>${img.downloads}</p>
-           </div>
-            </div>
-          </li>
-            `,
-        ''
-      );
-      // Виклик методу для оновлення галереї
-      lightbox.refresh();
-    })
-    .catch(error => {
-      ul.innerHTML = '';
-      input.value = '';
 
-      error = iziToast.show({
-        title: '',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        messageColor: '#FFFFFF',
-        backgroundColor: '#EF4040',
-        color: '#B51B1B',
-        iconUrl: './bi_x-octagon.svg',
-        iconColor: '#FAFAFB',
-        position: 'topRight',
-      });
-    });
+      // Вираховуємо останню сторінку
+      if (page >= Math.ceil(totalHits / per_page)) {
+        isLastPage = true;
+        //! Приховуємо кнопку Load More
+        loadMoreBtn.classList.add('is-hidden');
+      }
+      // Додаємо сторінки для Load More
+      page++;
+      // Повертаємо додану сторінку
+      return hits;
+    } catch (error) {
+      errorAlert();
+    }
+  };
+};
+
+// ============= /Пагінація ============ //
+
+// Для замикання функції виведення зображень
+let doFetch = null;
+
+form.addEventListener('submit', async event => {
+  event.preventDefault();
+
+  // Видаляємо слухача Load More, якщо він вже викликався до нового пошуку
+  if (doFetch != null) {
+    loadMoreBtn.removeEventListener('click', doFetch);
+    // !Приховуємо кнопку Load More
+    loadMoreBtn.classList.add('is-hidden');
+  }
+
+  // Беремо данні з поля введення форми
+  const data = new FormData(event.currentTarget);
+  const query = data.get('query');
+  // Чистимо сторінку і поле введення від попереднього запиту
+  ul.innerHTML = '';
+  input.value = '';
+
+  // Викликаємо запит
+  const fetchImages = createGetImagesRequest(query);
+
+  // Функція виведення зображень
+  doFetch = async () => {
+    // Чекаємо на зображення
+    const images = await fetchImages();
+
+    // Відмальовуємо зображення
+    renderImages(images);
+  };
+
+  await doFetch();
+
+  // Відображаємо кнопку Load More
+  loadMoreBtn.classList.remove('is-hidden');
+
+  // Додаємо слухача Load More з повторним викликом функції виведення зображень
+  loadMoreBtn.addEventListener('click', doFetch);
 });
